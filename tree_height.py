a536c21237ff7bb6f200788dab70a17ecb04268b
LIARSSS <arturs.apinis1@gmail.com>
2023-03-08 00:20:06 +0200

    ld2

--- tree_height.py
+++ tree_height.py
@@ -1,33 +1,69 @@
-# python3
-
 import sys
 import threading
-import numpy
 
 
-def compute_height(n, parents):
-    # Write this function
-    max_height = 0
-    # Your code here
-    return max_height
+class NodeClass:
+    def __init__(self, i):
+        self.i = i
+        self.parent = None
+        self.child = []
+
+    def add_child(self, child):
+        self.child.append(child)
+        child.parent = self
+
+
+def compute_height(n, pr):
+    nodesAll = [NodeClass(i) for i in range(n)]
+    root_i = -1
+
+    for i, parent_i in enumerate(pr):
+        if parent_i == -1:
+            root_i = i
+        else:
+            parent = nodesAll[parent_i]
+            parent.add_child(nodesAll[i])
+
+    root = nodesAll[root_i]
+    line = [root]
+    h = 0
+
+    while line:
+        lineArray = []
+        h += 1
+
+        for node in line:
+            lineArray.extend(node.child)
+
+        line = lineArray
+    return h
 
 
 def main():
-    # implement input form keyboard and from files
-    
-    # let user input file name to use, don't allow file names with letter a
-    # account for github input inprecision
-    
-    # input number of elements
-    # input values in one variable, separate with space, split these values in an array
-    # call the function and output it's result
-    pass
+    usersChoice = input().lower()
 
-# In Python, the default limit on recursion depth is rather low,
-# so raise it here for this problem. Note that to take advantage
-# of bigger stack, we have to launch the computation in a new thread.
-sys.setrecursionlimit(10**7)  # max depth of recursion
-threading.stack_size(2**27)   # new thread will get stack of such size
+    if usersChoice == 'i':
+        n = int(input())
+        pr = list(map(int, input().split()))
+    elif usersChoice == 'f':
+        fname = input()
+
+        if 'a' in fname:
+            return
+
+        try:
+            with open('data/' + fname, 'r') as f:
+                n = int(f.readline())
+                pr = list(map(int, f.readline().split()))
+        except Exception:
+            return
+    else:
+        return
+
+    print(compute_height(n, pr))
+
+
+sys.setrecursionlimit(10 ** 7)
+threading.stack_size(2 ** 27)
 threading.Thread(target=main).start()
 main()
-# print(numpy.array([1,2,3]))
